5521de500d43d1b775d739a9e82c9ed1db1c6a0f
hbbae <hbbae@wecruitcorp.com>
2021-12-07 16:30:41 +0900

    first

---
+++ src/main/kotlin/com/springcloud/eurekaclient/api/TestController.kt
@@ -0,0 +1,14 @@
+package com.springcloud.eurekaclient.api
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+
+    @GetMapping("/test")
+    fun test():ResponseEntity<String> {
+       return ResponseEntity.ok("TEST");
+    }
+}
